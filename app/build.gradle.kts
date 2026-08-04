f2c4b964c9de7d3891e6d48b380e2e9ad785175a
Rupin Munjal <157275712+rupinmunjal@users.noreply.github.com>
2026-08-04 18:36:15 -0400

    Step 3: Integrate secrets-gradle-plugin for secure API key management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.services)
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
